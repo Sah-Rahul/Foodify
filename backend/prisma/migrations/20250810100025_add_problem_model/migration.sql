29bed85a2b7cb5b521542caea97f5487c3f2ec3d
RahulSah <sahfamilyinfo@gmail.com>
2025-08-10 16:23:59 +0545

    add-problem-model

---
+++ backend/prisma/migrations/20250810100025_add_problem_model/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "public"."Difficulty" AS ENUM ('EASY', 'MEDIUM', 'HARD');
+
+-- CreateTable
+CREATE TABLE "public"."Problem" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "difficulty" "public"."Difficulty" NOT NULL,
+    "tags" TEXT[],
+    "userId" TEXT NOT NULL,
+    "examples" JSONB NOT NULL,
+    "constraints" TEXT NOT NULL,
+    "hints" TEXT,
+    "testCases" JSONB NOT NULL,
+    "codeSnippets" JSONB NOT NULL,
+    "referenceSolutions" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Problem_description_key" ON "public"."Problem"("description");
+
+-- AddForeignKey
+ALTER TABLE "public"."Problem" ADD CONSTRAINT "Problem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
